5f4f455a64585e84fa024fc89ee33a9332759fe7
Pankaj-prasad758 <prasadpankaj897@gmail.com>
2024-10-08 15:32:58 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,40 @@
+import { useDispatch } from "react-redux";
 import "./App.css";
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import authService from "./appwrite/auth/auth";
+import { login, logout } from "./store/authSlice";
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
+import { Outlet } from "react-router-dom";
 function App() {
-console.log(import.meta.env.VITE_APPWRITE_URL);
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService
+      .getcurrentuser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .catch()
+      .finally(() => setLoading(false));
+  }, []);
 
-  
-  
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">Daily blog</h1>
-
-    </>
-  );
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-600">
+ <div className="w-full block">
+  <Header />
+  <main>
+   T <Outlet />
+  </main>
+  <Footer />
+ </div>
+    </div>
+  ) : null;
 }
 
 export default App;
--- src/appwrite/auth/services.js
+++ src/appwrite/auth/services.js
@@ -35,6 +35,101 @@ export class Services {
         throw error
     }
   }
+
+  async updatePost(slug ,{title,  content, featureImage, status}){
+    try {
+      return await this.databases.updateDocument(
+        Config.appwriteDatabaseId,
+        Config.appwriteCollectionId,
+        slug,
+        {
+         title,
+         content,
+         status,
+         featureImage,
+         }
+      )
+    } catch (error) {
+      console.log("Appwrite services  :: updatePost error" , error);
+      
+    }
+  }
+
+  async deletePost(slug){
+  try {
+     await this.databases.deletePost(
+      Config.appwriteDatabaseId,
+      Config.appwriteCollectionId,
+      slug,
+    )
+    return true
+  } catch (error) {
+    console.log("Appwrite service:: deletePost error" , error);
+    return false
+  }
+  }
+  
+  async getPost(){
+    try {
+      
+      return await this.databases.getDocument(
+              Config.appwriteDatabaseId,
+              Config.appwriteCollectionId,
+              slug,
+      )
+    } catch (error) {
+      console.log("Appwrite service :: getPost :: error", error);
+      return false;
+
+    }
+  }
+
+  async getPosts(queries = [Query.equal("status","active")]){
+    try {
+      return await this.databases.listDocuments(
+        Config.appwriteDatabaseId,
+        Config.appwriteCollectionId,
+        queries,
+      )
+    } catch (error) {
+      console.log("Appwrite service :: getPosts :: error", error);
+      
+    }
+  }
+   async uploadFile(file){
+    try {
+      return await this.bucket.createFile(
+        Config.appwriteBucketId,
+        ID.unique(),
+        file,
+      )
+    } catch (error) {
+      console.log("Appwrite serive :: uploadFile :: error", error);
+      
+    }
+   }
+
+   async deleteFile(fileId){
+    try {
+      await this.bucket.deleteFile(
+        Config.appwriteBucketId,
+        fileId
+      )
+      return true
+    } catch (error) {
+      console.log("Appwrite service :: deleteFile :: error", error);
+      return false;
+    }
+   }
+
+     getFilePreview(fileId){
+      return this.bucket.getFilePreview(
+        Config.appwriteBucketId,
+        fileId
+      )
+
+     }
+   
 }
 
 const services = new Services();
